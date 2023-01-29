7657382dbae67236468fa568157b00999330925d
Dmitriy Cheblukov <cheba1256@gmail.com>
2023-01-29 20:55:22 +0300

    Edit task2

--- dec_easy.py
+++ dec_easy.py
@@ -18,7 +18,7 @@ def logger2(path):
     def __logger(old_function):
         def new_function(*args, **kwargs):
             nonlocal path
-            with open('path', 'a', encoding='utf-8') as log_file:
+            with open(path, 'a', encoding='utf-8') as log_file:
                 result = f'''\nДата и время запуска программы: {datetime.now()}
                                     Название функции: {old_function.__name__}
                                     Аргументы: {args} и {kwargs}
